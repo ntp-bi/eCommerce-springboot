640efcadbf8e11693a3afc520576c1649a9cdf82
ntp-bi <bintp22122002@gmail.com>
2024-10-26 20:24:01 +0700

    setup project

---
+++ src/main/java/com/ntp/ecommercespringboot/ECommerceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ntp.ecommercespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ntp/ecommercespringboot/ECommerceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntp.ecommercespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
